fc13a79e0952a339b7bba92650744e5c686c322a
Juan Gutierrez <juan.gutierrez@auro-group.com>
2022-11-07 18:22:39 +0100

    Initial commit

---
+++ LocalNotification/ContentView.swift
@@ -0,0 +1,47 @@
+//
+//  ContentView.swift
+//  LocalNotification
+//
+//  Created by Juan Gutierrez on 7/11/22.
+//
+
+import SwiftUI
+import UserNotifications
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Button("Request Permission") {
+                UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge,
+                    .sound]) { success, error in
+                        if success {
+                            print("All set!")
+                        } else if let error = error {
+                            print(error.localizedDescription)
+                        }
+                }
+            }
+            
+            Button("Schedule Notification") {
+                let content = UNMutableNotificationContent()
+                content.title = "Feed the dogs"
+                content.subtitle = "They look hungry"
+                content.sound = UNNotificationSound.default
+                
+                let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+                
+                let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+                
+                UNUserNotificationCenter.current().add(request)
+                
+                print("Notification received")
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
